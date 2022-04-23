1b1ea8f6d4e2fb09c1ad581781037c2b30064b14
pujacis <puja.v@cisinlabs.com>
2022-04-23 12:57:28 +0530

    added space

--- Assenment/Controllers/HomeController.cs
+++ Assenment/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace Assenment.Controllers
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+
         }
 
         public IActionResult Index()
