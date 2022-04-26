15534f6f4209e62eb3f2395cad52ac0920458255
pujacis <puja.v@cisinlabs.com>
2022-04-26 23:27:08 +0530

    Get Data Using Ajax and post also

--- Assenment/Controllers/BooksController.cs
+++ Assenment/Controllers/BooksController.cs
@@ -5,55 +5,59 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Assessment1.Controllers
 {
-    [Authorize(Roles = "Book Owner")]
+    //[Authorize(Roles = "Book Owner,Super Admin,Peopl")]
+    [Authorize]
     public class BooksController : Controller
     {
         TrainingDBContext db = new TrainingDBContext();
         [HttpGet]
-        [Authorize(Roles = "Peopl,Book Owner")]
+        [Authorize]
         public IActionResult GetBooks()
         {
-            List<PujaBook> booklist =db.PujaBooks.ToList();
+            List<PujaBook> booklist = db.PujaBooks.ToList();
 
             return View(booklist);
         }
         [HttpGet]
-        [Authorize(Roles = "Book Seller,Book Owner")]
+        [Authorize(Roles = "Book Seller,Book Owner,Super Admin")]
         public IActionResult AddBook()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult AddBook(PujaBook book)
+        [Authorize(Roles = "Book Seller,Book Owner,Super Admin")]
+        public IActionResult AddBooks(PujaBook book)
         {
             db.PujaBooks.Add(book);
             db.SaveChanges();
-            if (book!=null)
+            if (book != null)
             {
                 return RedirectToAction("GetBooks");
             }
             return View(book);
         }
         [HttpGet]
+        [Authorize(Roles = "Book Seller,Book Owner,Super Admin")]
         public IActionResult UpdateBook(int Id)
-        { 
-            var book =db.PujaBooks.Where(model=>model.Id==Id).FirstOrDefault();
+        {
+            var book = db.PujaBooks.Where(model => model.Id == Id).FirstOrDefault();
 
             return View(book);
         }
         [HttpPost]
+        [Authorize(Roles = "Book Seller,Book Owner,Super Admin")]
         public IActionResult UpdateBook(PujaBook book)
         {
             var bok = db.PujaBooks.Where(model => model.Id == book.Id).FirstOrDefault();
-            if(bok!=null)
+            if (bok != null)
             {
-                bok.Id= book.Id;
-                bok.BookName= book.BookName;
-                bok.Author= book.Author;
-                bok.Price= book.Price;
-                bok.ReleaseDate= book.ReleaseDate;
-                bok.Discount= book.Discount;
-                bok.Genre= book.Genre;
+                bok.Id = book.Id;
+                bok.BookName = book.BookName;
+                bok.Author = book.Author;
+                bok.Price = book.Price;
+                bok.ReleaseDate = book.ReleaseDate;
+                bok.Discount = book.Discount;
+                bok.Genre = book.Genre;
                 db.SaveChanges();
                 if (book != null)
                 {
@@ -62,9 +66,10 @@ namespace Assessment1.Controllers
             }
             return View(book);
         }
-         public IActionResult DeleteBook(int id)
+        [Authorize(Roles = "Book Seller,Book Owner,Super Admin")]
+        public IActionResult DeleteBook(int id)
         {
-            var book =db.PujaBooks.Where(model=>model.Id==id).FirstOrDefault(); 
+            var book = db.PujaBooks.Where(model => model.Id == id).FirstOrDefault();
             db.Entry(book).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
             db.SaveChanges();
             if (book != null)
@@ -73,12 +78,31 @@ namespace Assessment1.Controllers
             }
             return View();
         }
+        [Authorize(Roles = "Book Seller,Book Owner,Super Admin,Peopl")]
         public IActionResult BookDetails(int Id)
         {
             var book = db.PujaBooks.Where(model => model.Id == Id).FirstOrDefault();
 
             return View(book);
         }
+        public JsonResult GetBookList()
+        {
+            return Json(db.PujaBooks.ToList());
+        }
+        public JsonResult AddBook(PujaBook book)
+        {
+            string status = "success";
+            try
+            {
+                db.PujaBooks.Add(book);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                status = ex.Message;
+            }
+            return Json(status);
 
+        }
     }
 }
